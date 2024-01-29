6f335886a3ed38e2abb861ddeebfd0c5992f4635
Yiling Cai <yilingcai941116@gmail.com>
2024-01-29 21:45:36 +0100

    add projet 3 sqlite

--- BOOK/books2.py
+++ BOOK/books2.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI,Path,Query, HTTPException
 from pydantic import BaseModel, Field
 from typing import Optional
+from starlette import status
 
 app = FastAPI()
 
@@ -49,16 +50,55 @@ BOOKS = [
     Book(6, 'HP3', 'mimi 5', 'good book 5', 1)
 ]
 
-@app.get("/books")
+@app.get("/books", status_code=status.HTTP_200_OK)
 async def read_all_books():
     return BOOKS
 
-@app.post("/books/create_book")
+@app.get("/books/{book_id}")
+async def read_all_books(book_id: int = Path(gt=0)):
+    # for book in BOOKS:
+    #     if book.id == book_id:
+    #         return book
+    return next(book for book in BOOKS if book.id == book_id)
+    raise HTTPException(status_code=404, detail='Item not found')
+
+@app.get("/books/")
+async def read_book_by_rating(book_rating: int = Query(gt=0, lt=6)):
+    # book_to_return = []
+    # for i in range(len(BOOKS)):
+    #     if BOOKS[i].rating == book_rating:
+    #         book_to_return.append (BOOKS[i])
+
+    return list(book for book in BOOKS if book.rating == book_rating)
+
+
+@app.post("/books/create_book", status_code=status.HTTP_201_CREATED)
 async def create_book(request_book: BookRequest):
     new_book = Book(**request_book.dict())
     BOOKS.append(find_book_id(new_book))
 
 
+@app.put("/books/update_book/")
+async def update_book(book: BookRequest):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book.id:
+            updated_book = Book(**book.dict())
+            BOOKS[i] = updated_book
+            book_chanegd = True
+    if not book_changed:
+        raise HTTPException(status_code=404, detail = 'Item not found')
+
+@app.delete("/books/{book_id}")
+async def delete_book(book_id: int = Path(gt=0)):
+    book_changed = False
+    for i in range(len(BOOKS)):
+        if BOOKS[i].id == book_id:
+            BOOKS.pop(i)
+            book_changed = True
+            break
+    if not book_changed:
+        raise HTTPException(status_code=404, detail='Item not found')
 def find_book_id(book: Book):
     # if len(BOOKS) > 0:
     #     book.id = BOOKS[-1].id + 1
