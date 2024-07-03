19e612ac73e1f22eec6471b321c5241049118089
yiling <yilingcai941116@gmail.com>
2024-07-03 19:26:21 +0200

    add database.sql

---
+++ TodoApp/database.sql
@@ -0,0 +1,36 @@
+CREATE TABLE IF NOT EXISTS public.users(
+    id Integer,
+    email varchar(20) UNIQUE, 
+    username varchar(20) UNIQUE, 
+    first_name varchar(20), 
+    last_name varchar(20), 
+    hashed_password varchar(20), 
+    is_active boolean default True, 
+    role varchar(20),
+	PRIMARY KEY(id)
+	);
+
+
+CREATE TABLE IF NOT EXISTS public.todos
+(
+    id integer NOT NULL,
+    title character varying(20) COLLATE pg_catalog."default",
+    description character varying(20) COLLATE pg_catalog."default",
+    priority integer,
+    complete character varying(20) COLLATE pg_catalog."default",
+    owner_id integer,
+    CONSTRAINT todos_pkey PRIMARY KEY (id),
+    CONSTRAINT fk_users FOREIGN KEY (owner_id)
+        REFERENCES public.users (id) MATCH SIMPLE
+        ON UPDATE NO ACTION
+        ON DELETE NO ACTION
+);
+
+TABLESPACE pg_default;
+
+ALTER TABLE IF EXISTS public.todos
+    OWNER to dbrole;
+
+
+ALTER TABLE IF EXISTS public.users
+    OWNER to dbrole;
