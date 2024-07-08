f602737b0342b844949b141a734121f522411ed0
yiling <yilingcai941116@gmail.com>
2024-07-08 18:50:00 +0200

    add frontend and modify backend

--- TodoApp/main.py
+++ TodoApp/main.py
@@ -1,11 +1,14 @@
 from fastapi import FastAPI
-from  .models import Base 
-from .database import engine
-from .routers import auth, todos, admin, users
+from  models import Base 
+from database import engine
+from routers import auth, todos, users
+from starlette.staticfiles import StaticFiles
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+app.mount("/static", StaticFiles(directory='static'), name='static')
 
 
 @app.get("/healthy")
@@ -14,5 +17,4 @@ async def health_check():
 
 app.include_router(auth.router)
 app.include_router(todos.router)
-app.include_router(admin.router)
 app.include_router(users.router)
